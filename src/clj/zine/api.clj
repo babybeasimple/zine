8f4525c4630d2840e8408bbeaaeb062ad74f8ca0
babybeasimple <babybeasimple@gmail.com>
2014-04-19 17:33:15 +0400

    add memory atom based data storage

--- src/clj/zine/api.clj
+++ src/clj/zine/api.clj
@@ -2,8 +2,17 @@
   (:refer-clojure :exclude [defn])
   (:require [tailrecursion.castra :refer [defn]]))
 
+(def posts
+  [{:title   "Simple Man"
+    :content "And be a simple, kind of man. Oh be something, you love and understand. Baby be a simple, kind of man."}
+   {:title   "Champagne Supernova"
+    :content "Someday you will find me. Caught beneath the landslide. In a champagne supernova. A champagne supernova in the sky"}])
+
+(def initial-db-value
+  {:posts posts})
+
+(def db (atom initial-db-value))
+
 (defn get-state []
-  {:posts [{:title   "Simple Man"
-            :content "And be a simple, kind of man. Oh be something, you love and understand. Baby be a simple, kind of man."}
-           {:title   "Champagne Supernova"
-            :content "Someday you will find me. Caught beneath the landslide. In a champagne supernova. A champagne supernova in the sky"}]})
+  (let [db-val @db]
+    {:posts (rseq (:posts db-val))}))
